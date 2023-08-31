722a1b7582b9bd6387c1aab292446f2c7ec3dd39
Sean R Reid <sean@torchcodelab.com>
2023-08-31 14:09:11 -0400

    Added toggle class function

--- src/index.js
+++ src/index.js
@@ -1,25 +1,51 @@
 const submitMood = document.querySelector('#submitMood');
 
+function toggleClass(element, className, classesToRemove) {
+    console.log(element, className, classesToRemove);
+    if (element.classList.contains(className)) {
+        element.classList.remove(className);
+    } else {
+        element.classList.add(className);
+    }
 
-submitMood.addEventListener('click', function(event) {
-  event.preventDefault();
-  const moodInput = document.querySelector("[name=mood]:checked");
-  const myMood = document.querySelector("#myMood");
-  const moodValue = moodInput.value;
-  myMood.innerHTML = moodValue;
-  const classes = myMood.classList;
-  classes.toggle('default-mood');
-  switch(moodValue) {
-    case 'Very Happy' :
-      classes.toggle('very-happy-mood')
-      break
-    case 'Happy' :
-      classes.toggle('happy-mood')
-      break
-    case 'Not So Happy':
-      classes.toggle('not-so-happy-mood')
-      break
-    default:
-      console.log("IT'S A SWITCH")
-  }
-})
+    if (classesToRemove.length > 0) {
+        classesToRemove.forEach(function (className) {
+            if (element.classList.contains(className)) {
+                element.classList.remove(className);
+            }
+        });
+    }
+}
+
+submitMood.addEventListener('click', function (event) {
+    event.preventDefault();
+    const moodInput = document.querySelector('[name=mood]:checked');
+    const myMood = document.querySelector('#myMood');
+    const moodValue = moodInput.value;
+    myMood.innerHTML = moodValue;
+
+    toggleClass(myMood, 'default-mood', []);
+
+    switch (moodValue) {
+        case 'Very Happy':
+            toggleClass(myMood, 'very-happy-mood', [
+                'happy-mood',
+                'not-so-happy-mood',
+            ]);
+            break;
+        case 'Happy':
+            toggleClass(myMood, 'happy-mood', [
+                'very-happy-mood',
+                'not-so-happy-mood',
+            ]);
+            break;
+        case 'Not So Happy':
+            toggleClass(myMood, 'not-so-happy-mood', [
+                'happy-mood',
+                'very-happy-mood',
+            ]);
+            break;
+        default:
+            return;
+    }
+});
